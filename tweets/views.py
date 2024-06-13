5628e007cefd943b19c4c57f7e4f7f49872f2e4f
rogerchanac007 <armandoroger007@gmail.com>
2024-06-13 09:01:58 +0000

    firts views and models

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Tweet
 
 
-def tweet_detail_view(request):
-    return render(request, 'tweets/detail_view.html', {})
+def tweet_detail_view(request, id=1):
+    obj = Tweet.objects.get(id=id)
+    context = {
+        'object': obj
+    }
+    return render(request, 'tweets/detail_view.html', context)
 
 
 def tweet_list_view(request):
-    return render(request, 'tweets/list_view.html', {})
+    queryset = Tweet.objects.all()
+    context = {
+        'objects': queryset
+    }
+    return render(request, 'tweets/list_view.html', context)
